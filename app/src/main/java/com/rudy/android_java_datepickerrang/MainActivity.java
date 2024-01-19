d952d109e3aa026a2da57f322185b950e0db46d9
chaipat supharak <chaipat.sup@cpf.co.th>
2024-01-19 23:35:33 +0700

    first commit

---
+++ app/src/main/java/com/rudy/android_java_datepickerrang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rudy.android_java_datepickerrang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
