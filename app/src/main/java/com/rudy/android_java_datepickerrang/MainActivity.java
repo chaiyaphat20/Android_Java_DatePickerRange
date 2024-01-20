33c78d493153594bfb9501d2c204568eec8e81b7
chaipat supharak <chaipat.sup@cpf.co.th>
2024-01-20 21:58:10 +0700

    fix: date

--- app/src/main/java/com/rudy/android_java_datepickerrang/MainActivity.java
+++ app/src/main/java/com/rudy/android_java_datepickerrang/MainActivity.java
@@ -1,14 +1,67 @@
 package com.rudy.android_java_datepickerrang;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private TextView tvSelectDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn_show_picker);
+        tvSelectDate = findViewById(R.id.tv_select_date);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker<Pair<Long, Long>> picker = MaterialDatePicker.Builder.dateRangePicker()
+                        .setTitleText("เลือกวันที่")
+                        .setSelection(new Pair<>(MaterialDatePicker.thisMonthInUtcMilliseconds(), MaterialDatePicker.todayInUtcMilliseconds()))
+                        .build();
+
+                picker.show(MainActivity.this.getSupportFragmentManager(), "TAG");
+                picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+                    @Override
+                    public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                        tvSelectDate.setText(convertTimeToDate(selection.first) + " - " + convertTimeToDate(selection.second));
+                    }
+                });
+
+                picker.addOnNegativeButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    private String convertTimeToDate(Long time) {
+        Log.d("ART_GGG2", time.toString());
+        Calendar utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        utc.setTimeInMillis(time);
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        Log.d("ART_GGG1", format.format(utc.getTime()));
+        return format.format(utc.getTime());
     }
 }
